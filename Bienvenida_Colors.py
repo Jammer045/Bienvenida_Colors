8a67212dc53a42e6ea47554ae1763bb01abf982a
Jammer <jammer.08123@gmail.com>
2025-03-10 12:51:50 -0600

    My first commit

---
+++ Bienvenida_Colors.py
@@ -0,0 +1,16 @@
+color = input("Por favor, ingrese un color: ").lower()
+
+if color == "blanco" or color == "negro":
+    print("Te falta diseño")
+elif color == "verde":
+    print("Buenos días señor Hulk")
+elif color == "azul":
+    print("Buenos días señor Aquaman")
+elif color == "amarillo":
+    print("Buenos días señor Canario")
+elif color == "rojo":
+    print("Buenos días morenazo de fuego")
+elif color == "marron":
+    print("Bienvenido Mexicano")
+else:
+    print("Excelente elección señor, lo añadiremos al catálogo")
